f62feb65488dadba7cab032e5e2894a55285c273
[strodivaryann] <[sem.mm@mail.ru]>
2021-04-28 22:07:16 +1000

    hw2

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -7,9 +7,16 @@ def main(request):
     return render(request, 'mainapp/index.html')
 
 
-def products(request):
-    return render(request, 'mainapp/products.html')
-
-
 def contact(request):
     return render(request, 'mainapp/contact.html')
+
+
+def products(request):
+    links_menu = {'links': [
+        {'href': 'index', 'name': 'все'},
+        {'href': 'index', 'name': 'дом'},
+        {'href': 'index', 'name': 'офис'},
+        {'href': 'index', 'name': 'модерн'},
+        {'href': 'index', 'name': 'классика'},
+    ]}
+    return render(request, 'mainapp/products.html', context=links_menu)
