43e6747b931cc024eefe4746fa601474b58a3e6d
[strodivaryann] <[sem.mm@mail.ru]>
2021-05-05 22:08:40 +1000

    hw4

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from .views import main, products, contact
 
 urlpatterns = [
     path('', main, name='index'),
     path('products/', products, name='products'),
     path('contact/', contact, name='contact'),
+    path('auth/', include('authapp.urls', namespace='auth'))
 ]
