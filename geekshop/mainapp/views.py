f5a3d4473320ae06ad6376d5d869e07e30df9576
[strodivaryann] <[sem.mm@mail.ru]>
2021-05-13 22:55:29 +1000

    hw5

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import ProductCategory, Product
+from basketapp.models import Basket
 # Create your views here.
 
 
@@ -15,8 +16,39 @@ def contact(request):
     return render(request, 'mainapp/contact.html')
 
 
-def products(request):
+def products(request, pk=None):
+    title = 'продукты'
     links_menu = ProductCategory.objects.all()
-    product = Product.objects.get(id=2)
-    return render(request, 'mainapp/products.html', context={'links_menu': links_menu,
-                                                             'product': product})
+
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by('price')
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(
+                category__pk=pk).order_by('price')
+
+        context = {
+            'title': title,
+            'links_menu': links_menu,
+            'category': category,
+            'products': products,
+            'basket': basket,
+        }
+
+        return render(request, 'mainapp/products_list.html', context)
+
+    same_products = Product.objects.all()[3:5]
+
+    content = {
+        'title': title,
+        'links_menu': links_menu,
+        'same_products': same_products
+    }
+
+    return render(request, 'mainapp/products.html', content)
